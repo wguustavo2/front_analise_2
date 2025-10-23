6c64aef388a14e498672835351ddc8c4981bacc5
wguustavo2 <w.guustavo2@gmail.com>
2025-10-23 00:55:50 +0000

    Aula app praia e montanha

---
+++ Aulas/javascript/operadores.js
@@ -0,0 +1,34 @@
+/** OPERADORES ARITIMÉTICOS */
+// + _ * / % **
+/** OPERADORES RELACIONAIS */
+// > >= < <= !=
+
+// && || (E lógico, OU lógico)
+/*
+const ehpar = 3 || Number
+
+if(ehPar % 2 == 0) {
+// trata quando a condição for verdadeira
+alert("0 numero" + ehPar +" é par!")
+} else {
+    // trata a condição quando falsa
+    alert("0 numero" +ehPar + "é impar")
+}
+
+console.log(ehPar % 2== 0)
+*/
+//Pessoas com idade entre 18 e 70 anos, pode entrar
+//Pessoas maiores de 70 precisam de compania
+//Menores, vazam!
+
+const idade = 17
+
+if(idade >= 18){
+    alert("você tem" + idade +" anos \nVocê precisa de um acompanhante para entrar no baile")
+} else if(Idade > 70){
+    alert("17 não é 18!")
+    
+}
+    else {
+        alert("Por favor digite um número para idade")
+}
